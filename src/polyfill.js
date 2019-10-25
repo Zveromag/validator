bcb70ade62c6f75a7dcd7f4643d83a49f33ad6e3
Ivan Kuzmichov <ivan.kuzmichov@gmail.com>
2019-10-25 20:29:44 +0300

    updated closest polyfill

--- src/polyfill.js
+++ src/polyfill.js
@@ -1,24 +1,16 @@
-;(function (e) {
-  var matches = e.matches || e.matchesSelector || e.webkitMatchesSelector || e.mozMatchesSelector || e.msMatchesSelector || e.oMatchesSelector;
-  !matches ? (e.matches = e.matchesSelector = function matches(selector) {
-    var matches = document.querySelectorAll(selector);
-    var th = this;
-    return Array.prototype.some.call(matches, function (e) {
-      return e === th;
-    });
-  }) : (e.matches = e.matchesSelector = matches);
-})(Element.prototype);
+if (!Element.prototype.matches) {
+  Element.prototype.matches = Element.prototype.msMatchesSelector ||
+                              Element.prototype.webkitMatchesSelector;
+}
 
-(function (e) {
-  e.closest = e.closest || function (css) {
-    var node = this;
+if (!Element.prototype.closest) {
+  Element.prototype.closest = function(s) {
+    var el = this;
 
-    while (node) {
-      if (node.matches(css)) return node;
-      else node = node.parentElement;
-    }
+    do {
+      if (el.matches(s)) return el;
+      el = el.parentElement || el.parentNode;
+    } while (el !== null && el.nodeType === 1);
     return null;
-  }
-})(Element.prototype);
-
-export {};
+  };
+}
